ae661c072d396c4f7b64e9bde01fbcdd89c88a3c
Stacy Bridges <stcybrdgs@users.noreply.github.com>
2015-11-08 12:53:49 -0600

    Update app.js

--- angularMadLibs/app.js
+++ angularMadLibs/app.js
@@ -1,23 +1,11 @@
 
 // MODULE
-// the following variable is being put into the global namespace-- it's the 
-// only one we'll put there, and it's objective is to allow you to avoid
-// poluting the global namespace;
-// in the array brackets, you can include a list of the services/modules that your
-// app needs to use; link the service/module to your site per the script sources
-// cited in the html, and then insert the dependencies into the brackets in your
-// module's parameter list:
 var myApp = angular.module('myApp', []);
 
-// CONTROLLERS
-// here, we tell Angular what Model-View to bind together
-// the controller is named 'mainController,' and its code 
-// is defined in the function literal that is part of the declaration
+// CONTROLLER
 myApp.controller('mainController', ['$scope', function ($scope) {
 
-    $scope.handle = '';
-    $scope.option = '';    
-    
+   //MODEL
     $scope.placeName = '';
     $scope.noun = '';
     $scope.noun2 = '';
