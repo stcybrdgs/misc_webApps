08ad537050e73abce039ddfd5089ba09f042519e
Stacy Bridges <stacy.bridges@g.austincc.edu>
2015-11-08 18:58:00 -0600

    git commit -m "pushing updated angular mad libs files"

--- angularMadLibs/app.js
+++ angularMadLibs/app.js
@@ -1,11 +1,21 @@
+// jQuery --------------------------------------
+$(document).ready(function(){
+    $("span").html("&nbsp;&nbsp;&nbsp;&nbsp;");
+    
+    $("#reset").click(function(){
+        $("input").val(" ");  
+        $("span").html("&nbsp;&nbsp;&nbsp;&nbsp;");
+    });                  
+});
 
+// AngularJS --------------------------------------
 // MODULE
 var myApp = angular.module('myApp', []);
 
 // CONTROLLER
 myApp.controller('mainController', ['$scope', function ($scope) {
 
-   //MODEL
+    // MODEL
     $scope.placeName = '';
     $scope.noun = '';
     $scope.noun2 = '';
